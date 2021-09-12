47fc4b1d03899b8f5e61ed72ff9015302f4b7ec9
shruthishruu05 <shruthi.m@perfios.com>
2021-09-12 14:37:14 +0530

    ADDED : class with contact book array

---
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.addressbook;
+
+import java.util.Scanner;
+
+public class AddressBook {
+	Scanner sc = new Scanner(System.in);
+	private ContactPerson contactBook[];
+	private int numberOfConatcts=0;
+}
